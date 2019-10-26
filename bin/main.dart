facd982347e7b5ce3a5555fdb6ff983ebd575eb8
tb2706 <bierbaum.tim@outlook.de>
2019-10-26 20:46:36 +0200

    replace dio with http

--- bin/main.dart
+++ bin/main.dart
@@ -1,13 +1,14 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:http/http.dart' as http;
 
-import 'package:dio/dio.dart';
 import 'package:yaml/yaml.dart';
 
 fetchLanguagesYml() async {
   var data = null;
   try {
-    Response response = await Dio().get('https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml');
+    final response =
+        await http.get('https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml');
     data = json.decode(json.encode(loadYaml(response.toString())));
   } catch (e) {
     throw e;
@@ -16,14 +17,14 @@ fetchLanguagesYml() async {
 }
 
 void main() async {
-  var colors = new Map<String, String>();
+  var colors = Map<String, String>();
   var data = await fetchLanguagesYml();
 
   data.forEach((key, value) {
     colors[key] = value['color'];
   });
 
-  File file = new File('./lib/src/colors.dart');
+  File file = File('./lib/src/colors.dart');
 
   if (!await file.exists()) {
     file = await file.create();
